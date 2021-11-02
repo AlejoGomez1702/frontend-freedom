800284d36b9b3a90cd2eb823683db236bc359828
LUIS ALEJANDRO GOMEZ CASTANO <LAGOMCAS@davivienda.loc>
2021-11-02 15:13:07 -0500

    Iniciando la vista inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+export interface PeriodicElement {
+  position: number;
+  name: string;  
+  age: number;
+  dangerousness: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', age: 1.0079, dangerousness: 'H'},
+  {position: 2, name: 'Helium', age: 4.0026, dangerousness: 'He'},
+  {position: 3, name: 'Lithium', age: 6.941, dangerousness: 'Li'},
+  {position: 4, name: 'Beryllium', age: 9.0122, dangerousness: 'Be'}
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent 
+{
   title = 'freedom-frontend';
+  displayedColumns: string[] = ['position', 'name', 'age', 'dangerousness'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
+// Material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatTableModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
